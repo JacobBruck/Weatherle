5e6521ab8fd82cdf422d4ae61e9027694e06b586
Jacob Bruck <jpbruck23@gmail.com>
2026-08-04 22:44:46 -0400

    Revert "Revert the guess-dropdown/header pointer-events fix"
    This reverts commit 9ecbc60658ecc48dc839a6f79b2b7969a107aa75.

--- src/App.tsx
+++ src/App.tsx
@@ -157,7 +157,7 @@ function App() {
         <FaqModal user={user} onSignIn={signInWithGoogle} onSignOut={signOut} onClose={() => setShowFaq(false)} />
       )}
 
-      <div className={styles.header}>
+      <div className={`${styles.header} app-header`}>
         <span className={styles.modeBadge}>
           {isDaily ? '📅 Daily Challenge' : '♾️ Unlimited'}
         </span>
